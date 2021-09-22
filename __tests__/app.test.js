80740499e0bb914a805a6d41b4cee8d29816d995
Justin W Soto <soto@pdx.edu>
2021-09-22 15:56:00 -0700

    postgres working and first test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,5 @@
 const pool = require('../lib/utils/pool');
-const twilio = require('twilio');
+// const twilio = require('twilio');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
@@ -15,16 +15,20 @@ describe('03_separation-of-concerns-demo routes', () => {
     return setup(pool);
   });
 
-  it('creates a new order in our database and sends a text message', () => {
+  it('creates a new order (POST) in our database and sends a text message', async () => {
     return request(app)
       .post('/api/v1/orders')
       .send({ quantity: 10 })
       .then(res => {
-        // expect(createMessage).toHaveBeenCalledTimes(1);
         expect(res.body).toEqual({
           id: '1',
           quantity: 10
         });
       });
   });
+
+  /* CREATE TEST ROUTES FOR GET ALL, GET ID, PATCH, DELETE*/
+  // it('gets all orders in the database', async () => {
+
+  // });
 });
